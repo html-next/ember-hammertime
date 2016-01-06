f6a2e7cc8971987851506c2327773d14df471022
Chris Thoburn <runspired@gmail.com>
2016-01-05 20:11:16 -0600

    tests(touch-action): adds tests for basic touch-action support scenarios

---
+++ tests/dummy/app/components/click-component.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import layout from '../templates/components/click-component';
+
+export default Ember.Component.extend({
+  layout: layout,
+  click() {}
+});
---
+++ tests/dummy/app/components/input-component.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import layout from '../templates/components/input-component';
+
+export default Ember.Component.extend({
+  layout: layout
+});
