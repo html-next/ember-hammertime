d801d4a10b5ff2f370f6c2ed29d2ba0a4c2031a5
Chris Thoburn <runspired@gmail.com>
2016-01-05 20:08:31 -0600

    feat(app-import): import hammer-time and trigger ast-walker when addon is used in a project

--- index.js
+++ index.js
@@ -2,5 +2,33 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-hammertime'
+  name: 'ember-hammertime',
+
+  included: function (app) {
+    this._super.included.apply(this, arguments);
+
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    if (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+
+    if (!process.env.EMBER_CLI_FASTBOOT) {
+      app.import(app.bowerDirectory + '/hammer-time/hammer-time.js');
+    }
+  },
+
+  isDevelopingAddon: function() {
+    return true;
+  },
+
+  setupPreprocessorRegistry: function(type, registry) {
+    var TouchAction = require('./htmlbars-plugins/touch-action');
+
+    registry.add('htmlbars-ast-plugin', {
+      name: "touch-action",
+      plugin: TouchAction
+    });
+
+  }
+
 };
