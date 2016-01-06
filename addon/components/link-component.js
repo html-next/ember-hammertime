9b76ae4b3f8127262ec8a634202b5a15ccb6fc61
Chris Thoburn <runspired@gmail.com>
2016-01-05 20:07:26 -0600

    fix(touch-action-mixin): use reopen instead of reopenClass

--- addon/components/link-component.js
+++ addon/components/link-component.js
@@ -3,4 +3,4 @@ import TouchActionMixin from '../mixins/touch-action';
 
 let LinkComponent = Ember.LinkComponent || Ember.LinkView;
 
-export default LinkComponent.reopenClass(TouchActionMixin);
+export default LinkComponent.reopen(TouchActionMixin);
