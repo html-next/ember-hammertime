d061986d20c2e1bc34aca3c7a4373a25ff095067
Erik Trom <erik.trom@gmail.com>
2016-08-20 15:23:49 -0700

    Fix Error: `cannot use `attributeBindings` on a tag-less component`
    Modify touch-action mixin to smartly add `attributeBindings` to prevent
    assertion error from being thrown in ember beta/canary.
    
    Error is thrown when using 2.8.0-beta.3+cf714821 and 2.9.0-master+3f4ba8d4

--- addon/mixins/touch-action.js
+++ addon/mixins/touch-action.js
@@ -7,10 +7,19 @@ const {
 } = Ember;
 
 export default Mixin.create({
-  attributeBindings: ['touchActionStyle:style'],
+  init() {
+    this._super(...arguments);
+    if (this.tagName) {
+      this.attributeBindings = ['touchActionStyle:style'];
+      this.applyStyle = true;
+    } else {
+      this.applyStyle = false;
+    }
+  },
+
   touchActionStyle: computed(function() {
-    // we apply if click is present
-    let applyStyle = this.click;
+    // we apply if click is present and tagName is present
+    let applyStyle = this.applyStyle && this.click;
 
     if (!applyStyle) {
       // we apply if tagName
