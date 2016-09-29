14f6006cba47d44525054dda21b14b7fee33f91e
Erik Trom <erik.trom@gmail.com>
2016-09-28 21:04:07 -0700

    weird rebase conflict - same file, no changes, odd

--- addon/components/link-component.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-import TouchActionMixin from '../mixins/touch-action';
-
-let LinkComponent = Ember.LinkComponent;
-
-export default LinkComponent.reopen(TouchActionMixin);
