d45ff233da433f13121876af10044ea402229867
Robert Wagner <rwwagner90@gmail.com>
2018-10-16 08:19:17 -0400

    Fix lint

--- htmlbars-plugins/touch-action.js
+++ htmlbars-plugins/touch-action.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /* global module */
 /**
  An HTMLBars AST transformation that makes all instances of
@@ -14,25 +15,25 @@
  */
 
 // Set this to `false` to not apply the styles automatically to elements with an `action`
-var touchActionOnAction = true;
+let touchActionOnAction = true;
 // Remove 'onclick' if you do not want the styles automatically applied to elements with an `onclick`
-var touchActionAttributes = ['onclick'];
+let touchActionAttributes = ['onclick'];
 // Remove whichever element types you do not want automatically getting styles applied to them
-var touchActionSelectors = ['button', 'input', 'a', 'textarea'];
+let touchActionSelectors = ['button', 'input', 'a', 'textarea'];
 // The actual style string that is applied to the elements. You can tweak this if you want something different.
-var touchActionProperties = 'touch-action: manipulation; -ms-touch-action: manipulation; cursor: pointer;';
+let touchActionProperties = 'touch-action: manipulation; -ms-touch-action: manipulation; cursor: pointer;';
 
 function TouchActionSupport() {
   this.syntax = null;
 }
 
 TouchActionSupport.prototype.transform = function TouchActionSupport_transform(ast) {
-  var pluginContext = this;
-  var walker = new pluginContext.syntax.Walker();
+  let pluginContext = this;
+  let walker = new pluginContext.syntax.Walker();
 
   walker.visit(ast, function(node) {
     if (pluginContext.validate(node)) {
-      var style = elementAttribute(node, 'style');
+      let style = elementAttribute(node, 'style');
       if (!style) {
         style = {
           type: 'AttrNode',
@@ -49,14 +50,14 @@ TouchActionSupport.prototype.transform = function TouchActionSupport_transform(a
 };
 
 TouchActionSupport.prototype.validate = function TouchActionSupport_validate(node) {
-  var isFocusable;
+  let isFocusable;
 
   if (node.type === 'ElementNode') {
     isFocusable = touchActionSelectors.indexOf(node.tag) !== -1;
 
     if (isFocusable) {
       if (node.tag === 'input') {
-        var type = elementAttribute(node, 'type');
+        let type = elementAttribute(node, 'type');
         isFocusable = ['button', 'submit', 'text', 'file'].indexOf(type) !== -1;
       }
     }
@@ -74,8 +75,8 @@ TouchActionSupport.prototype.validate = function TouchActionSupport_validate(nod
  * @returns {*} The value of the attribute on the element
  */
 function elementAttribute(node, path) {
-  var attributes = node.attributes;
-  for (var i = 0, l = attributes.length; i < l; i++) {
+  let { attributes } = node;
+  for (let i = 0, l = attributes.length; i < l; i++) {
     if (attributes[i].name === path) {
       return attributes[i];
     }
@@ -85,9 +86,9 @@ function elementAttribute(node, path) {
 
 function elementModifierForPath(node, path) {
   // 1.11+ uses node.modifiers, and 1.10 uses node.helpers
-  var modifiers = node.modifiers || node.helpers;
-  for (var i = 0, l = modifiers.length; i < l; i++) {
-    var modifier = sexpr(modifiers[i]);
+  let modifiers = node.modifiers || node.helpers;
+  for (let i = 0, l = modifiers.length; i < l; i++) {
+    let modifier = sexpr(modifiers[i]);
 
     if (modifier.path.original === path) {
       return modifier;
@@ -107,8 +108,8 @@ function hasAction(node) {
     return false;
   }
 
-  var modifier = elementModifierForPath(node, 'action');
-  var onValue = modifier ? hashPairForKey(modifier.hash, 'on') : false;
+  let modifier = elementModifierForPath(node, 'action');
+  let onValue = modifier ? hashPairForKey(modifier.hash, 'on') : false;
 
   return modifier && (!onValue || onValue === 'click');
 }
@@ -123,8 +124,8 @@ function hasClick(node) {
 }
 
 function hashPairForKey(hash, key) {
-  for (var i = 0, l = hash.pairs.length; i < l; i++) {
-    var pair = hash.pairs[i];
+  for (let i = 0, l = hash.pairs.length; i < l; i++) {
+    let pair = hash.pairs[i];
     if (pair.key === key) {
       return pair;
     }
